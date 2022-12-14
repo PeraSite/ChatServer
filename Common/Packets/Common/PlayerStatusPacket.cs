9ad6f54ab154b09bc7588ad0d508a72b24f19c31
PeraSite <pretocki3@gmail.com>
2022-12-14 22:53:36 +0900

    feat: 플레이어 상태 알림 패킷 추가

---
+++ Common/Packets/Common/PlayerStatusPacket.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using Common.Objects;
+
+namespace Packets.Common;
+
+public enum PlayerStatusType : byte {
+	JOIN,
+	QUIT
+}
+
+public record PlayerStatusPacket(Player Player, PlayerStatusType Type) : IPacket {
+	public PlayerStatusPacket(BinaryReader reader) : this(
+		reader.ReadPlayer(),
+		(PlayerStatusType) reader.ReadByte()
+	) { }
+
+	public PacketType GetPacketType() {
+		return PacketType.Common_PlayerStatus;
+	}
+
+	public void Serialize(BinaryWriter writer) {
+		writer.Write(Player);
+		writer.Write((byte) Type);
+	}
+}
