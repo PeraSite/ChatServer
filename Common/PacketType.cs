890308e24262e504de7d443470168361346c2b60
PeraSite <pretocki3@gmail.com>
2022-12-20 22:31:23 +0900

    refactor: CreatePacket에서 Dictionary가 아니라 switch matching 문법을 사용하게 수정

--- Common/PacketType.cs
+++ Common/PacketType.cs
@@ -13,28 +13,28 @@ public enum PacketType : byte {
 	Client_PlayerList,
 	Server_PlayerList,
 
-	Common_PlayerStatus
+	Common_PlayerStatus,
 }
 
 public static class PacketTypes {
-	private static readonly Dictionary<PacketType, Func<BinaryReader, IPacket>> PACKET_CONSTRUCTORS = new() {
-		{PacketType.Client_Handshake, reader => new ClientHandshakePacket(reader)},
-		{PacketType.Server_Handshake, reader => new ServerHandshakePacket(reader)},
-
-		{PacketType.Client_Text, reader => new ClientTextPacket(reader)},
-		{PacketType.Server_Text, reader => new ServerTextPacket(reader)},
-
-		{PacketType.Client_PlayerList, _ => new ClientPlayerListPacket()},
-		{PacketType.Server_PlayerList, reader => new ServerPlayerListPacket(reader)},
-
-		{PacketType.Common_PlayerStatus, reader => new PlayerStatusPacket(reader)},
-	};
-
 	public static IPacket CreatePacket(this PacketType type, BinaryReader reader) {
-		return PACKET_CONSTRUCTORS[type](reader);
+		return type switch {
+			PacketType.Client_Handshake => new ClientHandshakePacket(reader),
+			PacketType.Server_Handshake => new ServerHandshakePacket(reader),
+
+			PacketType.Client_Text => new ClientTextPacket(reader),
+			PacketType.Server_Text => new ServerTextPacket(reader),
+
+			PacketType.Client_PlayerList => new ClientPlayerListPacket(),
+			PacketType.Server_PlayerList => new ServerPlayerListPacket(reader),
+
+			PacketType.Common_PlayerStatus => new PlayerStatusPacket(reader),
+
+			_ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+		};
 	}
 
 	public static TPacket CreatePacket<TPacket>(this PacketType type, BinaryReader reader) where TPacket : IPacket {
-		return (TPacket) PACKET_CONSTRUCTORS[type](reader);
+		return (TPacket) type.CreatePacket(reader);
 	}
 }
